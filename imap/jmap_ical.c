67b9e650adde4fe4a97024ebf518bacaadfebb00
Robert Stepanek <rsto@paranoia.at>
2020-08-06 17:08:37 +0200

    jmap_ical: remove deprecated particiantId property

--- imap/jmap_ical.c
+++ imap/jmap_ical.c
@@ -4731,8 +4731,7 @@ overrides_to_ical(icalcomponent *comp,
                     !strcmp(key, "recurrenceId") ||
                     !strcmp(key, "recurrenceRule") ||
                     !strcmp(key, "recurrenceOverrides") ||
-                    !strcmp(key, "replyTo") ||
-                    !strcmp(key, "participantId")) {
+                    !strcmp(key, "replyTo")) {
 
                     json_object_del(myoverride, key);
                 }
@@ -5115,8 +5114,6 @@ static void calendarevent_to_ical(icalcomponent *comp,
 
     /* replyTo and participants */
     participants_to_ical(comp, parser, event, oldcomps);
-
-    /* participantId: readonly */
 
     /* useDefaultAlerts */
     jprop = json_object_get(event, "useDefaultAlerts");
