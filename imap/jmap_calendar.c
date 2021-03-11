1a973e3db33e1c373709d472f3ceeda8f48531e1
Robert Stepanek <rsto@fastmailteam.com>
2021-03-11 16:22:10 +0100

    jmap_ical: guess IANA timezone name from VTIMEZONE

--- imap/jmap_calendar.c
+++ imap/jmap_calendar.c
@@ -3293,7 +3293,7 @@ static int getcalendarevents_cb(void *vrock, struct caldav_data *cdata)
         r = IMAP_INTERNAL;
         goto done;
     }
-    jstzones = jstimezones_new(ical);
+    jstzones = jstimezones_new(ical); // FIXME called twice, also in _tojmap below
 
     /* Convert to JMAP */
     jsevent = jmapical_tojmap(ical, NULL, &jmapctx);
@@ -3303,8 +3303,6 @@ static int getcalendarevents_cb(void *vrock, struct caldav_data *cdata)
         r = IMAP_INTERNAL;
         goto done;
     }
-    icalcomponent_free(ical);
-    ical = NULL;
 
     /* Add isDraft to cached event, we remove it later if not requested */
     mr = msgrecord_from_uid(rock->mailbox, cdata->dav.imap_uid);
